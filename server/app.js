86bf1470510c874e7ca9a9fb516f6bd98b3c14fd
Brandon Segelke <bbsegelke@gmail.com>
2023-03-10 19:59:36 -0500

    added button and form

--- server/app.js
+++ server/app.js
@@ -1,5 +1,7 @@
+
 const express = require('express');
 const path = require('path');
+//import { midjourney } from midjourney-client
 
 const PORT = 3000;
 const app = express();
@@ -8,12 +10,17 @@ const app = express();
 app.use(express.static(path.join(__dirname, '..', 'build')));
 
 // Route all other requests to your React application
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+});
 
 app.listen(PORT, () => {
+  
   console.log(`Server is now listening at port ${PORT}`);
 });
+
+
+
+
 
 console.log('test')
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,44 @@
 
-const App = () => <h1>Welcome to Monster Feeder Yo mama!!</h1>;
+import React from "react";
+//import midjourney from "midjourney-client";
+import axios from "axios";
+// const App = () => <h1>Welcome to Monster Feeder Yo mama!!</h1>;
 
+// export default App;
+
+class App extends React.Component{
+constructor(props){
+  super(props);
+
+this.state = {
+
+}
+
+
+this.imageGenerator = this.imageGenerator.bind(this);
+
+};
+
+
+imageGenerator(prompt) {
+  console.log('hi')
+}
+
+
+render(){
+
+return ( 
+<div>
+<h1>Welcome to Monster Feeder!!</h1>
+<input type="text" id="prompt" />
+<button onClick={this.generateImage}>Create Your Monster</button>
+<hr/>
+<img id="my-image"/>
+</div>
+)
+
+}
+
+
+};
 export default App;
